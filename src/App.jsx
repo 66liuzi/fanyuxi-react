e984e257ed845909ef63008694d758aedf48b322
66liuzi <164790801+66liuzi@users.noreply.github.com>
2026-06-24 18:13:56 +0800

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from './components/Hero';
+import Portfolio from './components/Portfolio';
+
+export default function App() {
+  return (
+    <div className="app">
+      <Hero />
+      <Portfolio />
+    </div>
+  );
+}
